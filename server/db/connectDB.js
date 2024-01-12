a1d2bdfb357800d780addebf801f3fc91ff25c44
soubankunnummel <muhammedsoubankunnummel123@gmail.com>
2024-01-12 17:34:47 +0530

    mogo connetion

--- server/db/connectDB.js
+++ server/db/connectDB.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
  const connectDB = async () => {
     try {
-        const connect = await mongoose.connect(process.env.MONGO_ATLAS_URI, { 
+      
+        const connect = await mongoose.connect("mongodb+srv://Muhammedsouban:souban10374@cluster0.it4k2gj.mongodb.net/talkflow?retryWrites=true&w=majority", { 
             useNewUrlParser: true,
             useUnifiedTopology: true,
             // useCreateIndex:true
